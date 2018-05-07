631dfee5978add2ec1e0457632eb42d64694c950
Marcelo Melo <marcelosdm@gmail.com>
2018-05-07 05:21:33 -0300

    Altera nomes dos arquivos

--- models.py
+++ models.py
@@ -1,23 +1,11 @@
 # -*- coding: UTF-8 -*-
 
 class Perfil(object):
-    'Classe padrão para perfis de usuários'
-
+    'Classe para moldar perfis de usuários'
     def __init__(self, nome, telefone, empresa):
         self.nome = nome
         self.telefone = telefone
         self.empresa = empresa
 
     def imprimir(self):
-        print 'Nome: %s, Telefone: %s, Empresa: %s' % (self.nome, self.telefone, self.empresa)
-
-class Data(object):
-    'Classe do desafio da aula 08, Exercício 11'
-    
-    def __init__(self, dia, mes, ano):
-        self.dia = dia
-        self.mes = mes
-        self.ano = ano
-
-    def imprime(self):
-        print '%s/%s/%s' % (self.dia, self.mes, self.ano)
+        print 'Nome: %s, Empresa: %s, Telefone: %s' % (self.nome, self.empresa, self.telefone)
