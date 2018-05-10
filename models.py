4fdb81295076cfdb973b2f02f8391718b4ea5978
Marcelo Melo <marcelosdm@gmail.com>
2018-05-10 05:21:04 -0300

    Adiciona classe Perfil_Vip e trabalha heranças

--- models.py
+++ models.py
@@ -2,10 +2,28 @@
 
 class Perfil(object):
     'Classe para moldar perfis de usuários'
+
     def __init__(self, nome, telefone, empresa):
         self.nome = nome
         self.telefone = telefone
         self.empresa = empresa
+        self.__curtidas = 0
 
     def imprimir(self):
         print 'Nome: %s, Empresa: %s, Telefone: %s' % (self.nome, self.empresa, self.telefone)
+
+    def curtir(self):
+        self.__curtidas+=1
+
+    def obter_curtidas(self):
+        return self.__curtidas
+
+class Perfil_Vip(Perfil):
+    'Classe para moldar perfis de usuários vips'
+
+    def __init__(self, nome, telefone, empresa, apelido):
+        super(Perfil_Vip, self).__init__(nome, telefone, empresa)
+        self.apelido = apelido
+
+    def obter_creditos(self):
+        return super(Perfil_Vip, self).obter_curtidas() * 10.0
